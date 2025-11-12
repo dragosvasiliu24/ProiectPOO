774b68fa8e7e228411acea97b28ac9b5b0874a36
mariancor21 <mariancornea173@gmail.com>
2025-11-12 12:58:41 +0200

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,75 @@
+package com.mycompany.electronice;
+
+public class Desktop extends Electronice implements Stare {
+    private String sistemOperare, culoare, sistemRacire;
+    private boolean gaming, placaNvidia;
+
+    public Desktop() {
+        super();
+        this.sistemOperare = "";
+        this.culoare = "";
+        this.sistemRacire = "";
+        this.gaming = false;
+        this.placaNvidia = false;
+    }
+    public Desktop(String marca,String numeProdus,double pret,boolean disponibil,String sistemOperare, String culoare, String sistemRacire) {
+        super(marca,numeProdus ,pret,disponibil);
+        this.sistemOperare = sistemOperare;
+        this.culoare = culoare;
+        this.sistemRacire = sistemRacire;
+        this.gaming = false;
+        this.placaNvidia = false;
+    }
+    public Desktop(Desktop original){
+        super(original);
+        this.sistemOperare = original.sistemOperare;
+        this.culoare = original.culoare;
+        this.sistemRacire = original.sistemRacire;
+        this.gaming = original.gaming;
+        this.placaNvidia = original.placaNvidia;
+    }
+    public void setSistemOperare(String sistemOperare) {
+        this.sistemOperare = sistemOperare;
+    }
+    public String getSistemOperare() {
+        return sistemOperare;
+    }
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+    public String getCuloare() {
+        return culoare;
+    }
+    public void setSistemRacire(String sistemRacire) {
+        this.sistemRacire = sistemRacire;
+    }
+    public String getSistemRacire() {
+        return sistemRacire;
+    }
+    public void setGaming(boolean gaming) {
+        this.gaming = gaming;
+    }
+    public boolean getGaming() {
+        return gaming;
+    }
+    public void setPlacaNvidia(boolean placaNvidia) {
+        this.placaNvidia = placaNvidia;
+    }
+    public boolean getPlacaNvidia() {
+        return placaNvidia;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+ "sistemOperare=" + sistemOperare+ "culoare"+ culoare+ "sistemRacire=" + sistemRacire+"gaming=" + gaming+ "placaNvidia=" + placaNvidia;
+    }
+    @Override
+    public void nou(){
+        System.out.println(" com.mycompany.electronice.Desktop nou");
+    }
+
+    @Override
+    public void resigilat(){
+        System.out.println(" com.mycompany.electronice.Desktop resigilat");
+    }
+
+}
---
+++ Laptop.java
@@ -0,0 +1,87 @@
+package com.mycompany.electronice;
+import com.mycompany.electronice.Electronice;
+import com.mycompany.electronice.Stare;
+
+public class Laptop extends Electronice implements Stare {
+    private String tipProcesor;
+    private String sistemOperare;
+    private int diagonalaDisplay , rataRefresh, memorie;
+    private  boolean touchscreen;
+
+    public Laptop() {
+        super();
+        this.tipProcesor = "";
+        this.sistemOperare = "";
+        this.diagonalaDisplay = 0;
+        this.rataRefresh = 0;
+        this.memorie = 0;
+        this.touchscreen = false;
+    }
+    public Laptop(String marca, String numeProdus, double pret, boolean disponibil,String tipProcesor, String sistemOperare, int diagonalaDisplay, int rataRefresh, int memorie, boolean touchscreen) {
+        super(marca,numeProdus,pret,disponibil);
+        this.tipProcesor = tipProcesor;
+        this.sistemOperare = sistemOperare;
+        this.diagonalaDisplay = diagonalaDisplay;
+        this.rataRefresh = rataRefresh;
+        this.memorie = memorie;
+        this.touchscreen = touchscreen;
+    }
+    public Laptop(Laptop original) {
+        super(original);
+        this.tipProcesor = original.tipProcesor;
+        this.sistemOperare = original.sistemOperare;
+        this.diagonalaDisplay = original.diagonalaDisplay;
+        this.rataRefresh = original.rataRefresh;
+        this.memorie = original.memorie;
+        this.touchscreen = original.touchscreen;
+    }
+    public void setTipProcesor(String tipProcesor) {
+        this.tipProcesor = tipProcesor;
+    }
+    public String getTipProcesor() {
+        return tipProcesor;
+    }
+    public void setSistemOperare(String sistemOperare) {
+        this.sistemOperare = sistemOperare;
+    }
+    public String getSistemOperare() {
+        return sistemOperare;
+    }
+    public void setDiagonalaDisplay(int diagonalaDisplay) {
+        this.diagonalaDisplay = diagonalaDisplay;
+    }
+    public int getDiagonalaDisplay() {
+        return diagonalaDisplay;
+    }
+    public void setRataRefresh(int rataRefresh) {
+        this.rataRefresh = rataRefresh;
+    }
+    public int getRataRefresh() {
+        return rataRefresh;
+    }
+    public void setMemorie(int memorie) {
+        this.memorie = memorie;
+    }
+    public int getMemorie() {
+        return memorie;
+    }
+    public void setTouchscreen(boolean touchscreen) {
+        this.touchscreen = touchscreen;
+    }
+    public boolean getTouchscreen() {
+        return touchscreen;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+ "tipProcesor"+tipProcesor+"sistemOperare"+sistemOperare+"diagonala display"+ diagonalaDisplay+"rata refresh"+ rataRefresh+"memorie"+memorie+"touchscreen"+touchscreen;
+    }
+    @Override
+    public void nou(){
+        System.out.println("laptop nou.");
+    }
+    @Override
+    public void resigilat(){
+        System.out.println(" laptop resigilat");
+    }
+
+}
